24f668e32cf1b2fa1a4924bb0b8d3a466107bc4a
Specnazko <drashpul2011@gmail.com>
2019-10-04 07:07:59 +0300

    add navigation for image

--- public/js/users.js
+++ public/js/users.js
@@ -1,5 +1,5 @@
 'use strict'
-
+const userImg = document.querySelector('.images-container');
 const user = window.location.pathname.split("/")[window.location.pathname.split("/").length-2];
 
 
@@ -20,12 +20,11 @@ function loadContent () {
         request.then(
             result => {
                 userData = JSON.parse(result);
-                console.log(userData);
 
                 const image = document.querySelector('.image');
                 userData.images.forEach (function (e){
                 
-                document.querySelector('.images-container').insertAdjacentHTML('beforeend', `<img src="../${userData.id}/img/${e}" alt="" class="image">`);
+                document.querySelector('.images-container').insertAdjacentHTML('beforeend', `<div class="image-wrapper"><img src="../${userData.id}/img/${e}" alt="" class="image"></div>`);
                 
                 });
 
@@ -37,3 +36,47 @@ function loadContent () {
     load ();
 }
 loadContent();
+
+
+function openImg (e) {
+    if (e.target.matches('.image')) {
+        const imgWrapper = document.querySelector('.open-img-wrapper');
+        const imgO = document.querySelector('.open-img');
+        const img = document.querySelector('.full-img');
+        const src = e.target.getAttribute('src');
+        img.setAttribute('src', src);
+        imgWrapper.style.display = 'flex';
+        imgO.style.display = 'flex';
+        const arrImg = document.querySelectorAll('.image');
+        let index;
+        for (let i=0; i<arrImg.length; i++) {
+            if (src == arrImg[i].getAttribute('src')) index = i;
+        }
+
+        console.log(index);
+        function navigationImg (e) {
+            
+            if (e.target.matches('.next-img')){
+
+                index++;
+                if (index == arrImg.length) index=0;
+                img.setAttribute('src', arrImg[index].getAttribute('src'));
+
+            }else if ( e.target.matches('.early-img')) {
+
+                index--;
+                if (index == -1) index=arrImg.length-1;
+                img.setAttribute('src', arrImg[index].getAttribute('src'));
+
+            }else if (!e.target.matches('.full-img')) {
+                imgWrapper.style.display = 'none';
+                imgO.style.display = 'none';
+            }
+        }
+        imgWrapper.addEventListener ('click', navigationImg);
+
+    }
+    return;
+}
+
+userImg.addEventListener('click', openImg);
